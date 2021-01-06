066f93b15b67e56d2d779a1d43bc9b3b29e2350d
Hoybaby <mbartek436@gmail.com>
2021-01-06 01:56:41 -0500

    added the jumbo component

---
+++ components/Jumbo/Jumbo.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const Jumbotron = () => {
+    return (
+        <div>
+            <div className="jumbotron jumbotron-fluid">
+                <div class="container">
+                    <h1 className="display-4">Fluid jumbotron</h1>
+                    <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Jumbotron;
