781b5f748060993dbc1040a56e1188d5238bb0e9
Hoybaby <mbartek436@gmail.com>
2021-01-06 09:44:33 -0500

    fixed an issue with class being named in properly with jsx

--- components/Jumbo/Jumbo.js
+++ components/Jumbo/Jumbo.js
@@ -5,7 +5,7 @@ const Jumbotron = () => {
     return (
         <div>
             <div className="jumbotron jumbotron-fluid">
-                <div class="container">
+                <div className="container">
                     <h1 className="display-4">Fluid jumbotron</h1>
                     <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
                 </div>
