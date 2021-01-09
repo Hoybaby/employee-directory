e7b4ef1f24aea7436c182a6bb9d1ed762638dcd6
Hoybaby <mbartek436@gmail.com>
2021-01-08 23:00:46 -0500

    trying to set up the api that was given to generate random users. Also created a striped table that was retrieved from Bootstrap itself and added to the app.js with proepr JSx syntax

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Jumbotron from './components/Jumbo/Jumbo';
 import Search from './components/Search/Search';
+import TableBootstrap from './components/Table/Table';
+
 
 function App() {
   return (
     <div>
       <Jumbotron/>
       <Search/>
+      <TableBootstrap/>
       
     </div>
   );
