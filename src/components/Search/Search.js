40b43442247c84b76d032d66482460004b2840ae
Hoybaby <mbartek436@gmail.com>
2021-01-20 00:33:40 -0500

    changing how i am doing this homework in a way that is simplier than before. I was overcimplicating it and saw an easier way after some reasearch

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,21 +3,15 @@ import React from "react";
 
 
 
+
 function Search() {
     return (
-        <div>
-            <form >
-                <input>
-                </input>
-
-                <button>
-                    Search
-                </button>
+        <div class="container-fluid-center">
+            <form class='d-flex'>
+                <input name="search" class="from-control me-2" type="search" placeholder="Search" value={props.search} onChange={props.handleInputChance}/>
             </form>
         </div>
+
     )
 }
-
 export default Search;
-
-
