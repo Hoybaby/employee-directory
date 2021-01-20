c3d25f451707e9daa2c428d2d02aeb523d9eebf0
Hoybaby <mbartek436@gmail.com>
2021-01-20 10:24:20 -0500

    adding more code to actually filter some of the code but have som errors

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,42 +1,40 @@
 // import API from "../../utils/API";
+import axios from 'axios';
 import React from 'react';
 
 
-const TableBootstrap = () => {
-  
-    return (
-        <table className="table table-striped">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Image</th>
-                    <th scope="col" >Name</th>
-                    <th scope="col">Phone</th>
-                    <th scope="col">Email</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>email1</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>email2</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>email3</td>
-                </tr>
-            </tbody>
-        </table>
-    );
-}
+class Table extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            employees: [],
+            search: "",
+            sortDirection: 0,
+        };
+    }
 
-export default TableBootstrap;
+    //With the help from Vlad and TA they showed me 20-07 activity had a lot.
+
+
+    componentDidMount() {
+        axios.get("https://randomuser.me/api/?results=25&nat=us")
+            .then((res)=> {
+                this.setState({ employees: res.data.results});
+            });
+    }
+
+    handleInputChange =(event) => {
+        let { name, value } = event.target;
+        this.setState({
+            [name] : value,
+        });
+    };
+
+
+
+
+
+
+
+
+}
