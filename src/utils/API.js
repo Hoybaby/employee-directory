bb9f47958849506a4fa2cb5d52865acb33ccee0a
Hoybaby <mbartek436@gmail.com>
2021-01-19 20:56:46 -0500

    removed a folder that i realized that i made a copy and didnt belong outside the src

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 
 
-
-
-
-
 export default function getEmplyoeeName(query) {
     const BASEURL = "https://randomuser.me/api/?results=200&nat=us";
     return axios.get(BASEURL);
